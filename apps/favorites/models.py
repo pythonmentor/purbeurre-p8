dc2c5a642373b71e51c43e46996c6b6b2b78ed20
Thierry Chappuis <tchappui@gmail.com>
2020-11-15 17:20:46 +0100

    Implemented the Favorite model

--- apps/favorites/models.py
+++ apps/favorites/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Favorite(models.Model):
+    """Favori enregistré par l'utilisateur pour substituer un produit."""
+
+    substitute = models.ForeignKey(
+        'products.Product',
+        on_delete=models.CASCADE,
+        related_name="favorites_as_substitute",
+    )
+    product = models.ForeignKey(
+        'products.Product',
+        on_delete=models.CASCADE,
+        related_name="favorites_as_product",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="favorites",
+    )
+
+    class Meta:
+        verbose_name_plural = 'favorites'
+        unique_together = ('substitute', 'product', 'user')
+
+    def __str__(self):
+        return f"({user.name}, {product.name}, {substitute.name})"
