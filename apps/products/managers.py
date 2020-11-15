1f90de093f9aa7418c601bc2566db5506cdee325
Thierry Chappuis <tchappui@gmail.com>
2020-11-15 16:17:55 +0100

    Implemented find_substitutes

--- apps/products/managers.py
+++ apps/products/managers.py
@@ -17,5 +17,50 @@ class ProductManager(models.Manager):
 
             # Création des catégories et association avec le produit
             for category_name in categories:
-                category, _ = Category.objects.get_or_create(name=category_name)
+                category, _ = Category.objects.get_or_create(
+                    name=category_name
+                )
                 product.categories.add(category)
+
+    def find_substitutes(self, product_name):
+        """Recherche des substituts à product_name.
+
+        La stratégrie utilisée est de rechercher les produits de meilleur
+        nutriscore avec le plus grand nombre de catégories en commun avec
+        product_name.
+
+        """
+        # Rechercher le produit correspondant à product_name en base
+        # de données
+        print(f"Recu: {product_name}")
+        product = (
+            self.filter(name__icontains=product_name).order_by('?').first()
+        )
+        print(f"Trouvé: {product}")
+        if not product:
+            return []
+
+        # On retourne une liste de substituts plus sains à product
+        substitutes = (
+            self.exclude(pk=product.pk)
+            .filter(
+                categories__in=product.categories.all(),
+                nutriscore__lt=product.nutriscore,
+            )
+            .annotate(num_common_categories=models.Count('pk'))
+            .order_by('-num_common_categories', 'nutriscore')
+        )
+        if substitutes:
+            return substitutes
+
+        # Si aucun produit plus sain n'est trouvé, on recherche des produits
+        # de même nutriscore
+        return (
+            self.exclude(pk=product.pk)
+            .filter(
+                categories__in=product.categories.all(),
+                nutriscore=product.nutriscore,
+            )
+            .annotate(num_common_categories=models.Count('pk'))
+            .order_by('-num_common_categories')
+        )
