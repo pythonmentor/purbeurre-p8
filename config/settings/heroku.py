67900041e0cd6105b71fde89acd1b145c94518fa
Thierry Chappuis <tchappui@gmail.com>
2020-11-23 13:21:17 +0100

    Updated EMAIL_PORT to 587

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -9,7 +9,7 @@ DEBUG = False
 # Configuration de l'email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gokujj@pythonclassmates.org'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
