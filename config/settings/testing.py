9e43b8263cf182e1b851dda2638b6fc206d5d1df
Thierry Chappuis <tchappui@gmail.com>
2020-11-24 12:18:16 +0100

    Configured the projet for functional testing

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,1 +1,7 @@
 from .base import *
+
+CHROMEDRIVER_PATH = str(BASE_DIR / 'webdrivers' / 'chromedriver')
+GECKODRIVER_PATH = str(BASE_DIR / 'webdrivers' / 'geckodriver')
+
+CHROMEDRIVER_OPTIONS = []  # ['--headless', 'window-size=1920x1080']
+GECKODRIVER_OPTIONS = []  # ['-headless']
